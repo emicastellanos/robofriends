506ae4df4c43ee9e51aba3b163735719ef791b3f
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2019-01-31 03:03:20 -0300

    Lista de robots e iteracion sobre cada card

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ( {id,name,email} ) => {
+//const id,name,email = props; (podria hacer eso si recibiera props por parametro. 
+//Pero tambien se puede deseccionar dentro de los parametros and it is even cleaner)
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt = 'robotito' src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/CardList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    // la consola del chrome dice: "Warning: Each child in an array or iterator should have a unique "key" prop
+    //entonces podemos agregar una prop key unica (por ejemplo el id)
+    
+    // const cardComponent = robots.map( (user,index) => {
+    //     return (
+    //         <Card 
+    //         key={index} 
+    //         id={robots[index].id} 
+    //         name={robots[index].name} 
+    //         email={robots[index].email} />
+    //     )
+    // });
+    //Pero mas cheto y mas cleaner si lo hacemos asi:
+    return (
+        <div>
+            {
+                robots.map ( (user,index) => {
+                    return (
+                        <Card
+                        key={index} 
+                        id={robots[index].id} 
+                        name={robots[index].name} 
+                        email={robots[index].email} 
+                        />
+                        );
+                    }) // en realidad va entre llaves porque es javascript dentro de html
+            }
+        </div>
+    );
+}
+
+export default CardList;
